d350aacaa753c077c039b419110f0e96ef19b34b
BambooCode98 <jw.thomas98@yahoo.com>
2022-02-23 17:42:42 -0600

    finished website until further ideas might be implemented

--- src/contact.js
+++ src/contact.js
@@ -4,12 +4,19 @@ const page = document.querySelector('#page');
 const contact = document.createElement('div');
 const contactHeader = document.createElement('h3');
 const contactInfo = document.createElement('div');
-const message = document.createElement('p');
+const message1 = document.createElement('div');
+const text1 = document.createElement('p');
+const text2 = document.createElement('p');
+const text3 = document.createElement('p');
+
+
 
 //add classes
 contactHeader.classList.add('headerMenu');
 contact.classList.add('page2');
-message.classList.add('contactmessage');
+message1.classList.add('contactmessage');
+
+contactInfo.classList.add('info3');
 
 //add the page heading
 contactHeader.textContent = "Contact";
@@ -18,4 +25,10 @@ contact.append(contactHeader);
 
 //page information
 contact.append(contactInfo);
-contactInfo.append(message);
+contactInfo.append(message1);
+text1.textContent = "Phone Number: 999-999-9999";
+text2.textContent = "Email: Bamboo@bamboo.com";
+text3.textContent = "Address: Bamboo Boulevard, Bamboo City, Bamboo Country";
+
+
+message1.append(text1, text2, text3);
