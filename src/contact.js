f8fba448f0ba8afef836380ba58cad2cfe980882
BambooCode98 <jw.thomas98@yahoo.com>
2022-02-23 14:43:32 -0600

    finished catalogue section

--- src/contact.js
+++ src/contact.js
@@ -4,9 +4,12 @@ const page = document.querySelector('#page');
 const contact = document.createElement('div');
 const contactHeader = document.createElement('h3');
 const contactInfo = document.createElement('div');
+const message = document.createElement('p');
 
+//add classes
 contactHeader.classList.add('headerMenu');
 contact.classList.add('page2');
+message.classList.add('contactmessage');
 
 //add the page heading
 contactHeader.textContent = "Contact";
@@ -15,3 +18,4 @@ contact.append(contactHeader);
 
 //page information
 contact.append(contactInfo);
+contactInfo.append(message);
